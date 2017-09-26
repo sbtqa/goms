260d79fcd9dbcf0a0471865288a503dd1edbcfd3
Konstantin Maltsev <mkypers@gmail.com>
2017-09-26 14:06:16 +0700

    undone handling methods

--- src/main/java/ru/sbtqa/tag/goms/processing/Processor.java
+++ src/main/java/ru/sbtqa/tag/goms/processing/Processor.java
@@ -19,23 +19,78 @@ public class Processor {
         for (Token token : tokens) {
             switch (token.getOperator()) {
                 case O:
-                    workflow.addAll(handleO(token));
+                    String pageName = Regex.get(token.getDescription(), "\"[\\w\\s]+\"");
+                    token.setDescription("Экран " + pageName);
+
+                    workflow.addAll(Wrapper.wrap(token));
+                    workflow.add(new Token(Symbol.M));
                     break;
 
                 case F:
-                    handleF(token);
+                    // init focused element 
+                    focusedElement = Regex.get(token.getDescription(), "\"[\\w\\s]+\"").replace("\"", "");
                     break;
 
                 case BB:
-                    workflow.addAll(handleBB(token));
+                    // insert M operator before BB 
+                    if (workflow.get(workflow.size() - 1).getOperator() != Symbol.M) {
+                        workflow.add(new Token(Symbol.M));
+                    }
+
+                    if (handContext != HandContext.ON_MOUSE) {
+                        workflow.add(new Token(Symbol.H));
+                        handContext = HandContext.ON_MOUSE;
+                        workflow.add(new Token(Symbol.M));
+                    }
+                    workflow.add(new Token(Symbol.P));
+                    workflow.addAll(Wrapper.wrap(token));
+                    workflow.add(new Token(Symbol.T));
                     break;
 
                 case KK:
-                    workflow.addAll(handleKK(token));
+                    if (!"".equals(focusedElement) && token.getDescription().contains(focusedElement)) {
+                        if (workflow.get(workflow.size() - 1).getOperator() != Symbol.M) {
+                            workflow.add(new Token(Symbol.M, "в фокусе элемент \"" + focusedElement + "\""));
+                        } else {
+                            workflow.set(workflow.size() - 1, workflow.get(workflow.size() - 1).setDescription("в фокусе элемент \"" + focusedElement + "\""));
+                        }
+                    } else {
+                        // insert M operator before KK 
+                        if (workflow.get(workflow.size() - 1).getOperator() != Symbol.M) {
+                            workflow.add(new Token(Symbol.M));
+                        }
+
+                        // focus on text field by mouse 
+                        if (handContext != HandContext.ON_MOUSE) {
+                            workflow.add(new Token(Symbol.H));
+                            handContext = HandContext.ON_MOUSE;
+                            workflow.add(new Token(Symbol.M));
+                        }
+                        workflow.add(new Token(Symbol.P));
+                        workflow.add(new Token(Symbol.BB));
+                        workflow.add(new Token(Symbol.M));
+                        workflow.add(new Token(Symbol.H));
+                        handContext = HandContext.ON_KEYBOARD;
+                        workflow.add(new Token(Symbol.M));
+                    }
+                    token.setMultiplier(Regex.get(token.getDescription(), "\"[\\w\\s]+\"").replace("\"", "").length());
+
+                    workflow.addAll(Wrapper.wrap(token));
                     break;
 
                 case K:
-                    workflow.addAll(handleK(token));
+                    if (workflow.get(workflow.size() - 1).getOperator() != Symbol.M) {
+                        workflow.add(new Token(Symbol.M));
+                    }
+
+                    if (handContext != HandContext.ON_KEYBOARD) {
+                        workflow.add(new Token(Symbol.H));
+                        handContext = HandContext.ON_KEYBOARD;
+                        workflow.add(new Token(Symbol.M));
+                    }
+
+                    workflow.addAll(Wrapper.wrap(token));
+                    workflow.add(new Token(Symbol.T));
                     break;
 
                 default:
@@ -44,95 +99,5 @@ public class Processor {
         }
 
         return workflow;
-    }
-
-    private static List<Token> handleO(Token token) {
-        List<Token> workflowPortion = new ArrayList<>();
-
-        String pageName = Regex.get(token.getDescription(), "\"[\\w\\s]+\"");
-        token.setDescription("Экран " + pageName);
-
-        workflowPortion.addAll(Wrapper.wrap(token));
-        workflowPortion.add(new Token(Symbol.M));
-
-        return workflowPortion;
-    }
-
-    private static List<Token> handleBB(Token token) {
-        List<Token> workflowPortion = new ArrayList<>();
-
-        // insert M operator before BB
-        if (workflowPortion.get(workflowPortion.size() - 1).getOperator() != Symbol.M) {
-            workflowPortion.add(new Token(Symbol.M));
-        }
-
-        if (handContext != HandContext.ON_MOUSE) {
-            workflowPortion.add(new Token(Symbol.H));
-            handContext = HandContext.ON_MOUSE;
-            workflowPortion.add(new Token(Symbol.M));
-        }
-        workflowPortion.add(new Token(Symbol.P));
-        workflowPortion.addAll(Wrapper.wrap(token));
-        workflowPortion.add(new Token(Symbol.T));
-
-        return workflowPortion;
-    }
-
-    private static List<Token> handleKK(Token token) {
-        List<Token> workflowPortion = new ArrayList<>();
-
-        if (!"".equals(focusedElement) && token.getDescription().contains(focusedElement)) {
-            if (workflowPortion.get(workflowPortion.size() - 1).getOperator() != Symbol.M) {
-                workflowPortion.add(new Token(Symbol.M, "в фокусе элемент \"" + focusedElement + "\""));
-            } else {
-                workflowPortion.set(workflowPortion.size() - 1, workflowPortion.get(workflowPortion.size() - 1).setDescription("в фокусе элемент \"" + focusedElement + "\""));
-            }
-        } else {
-            // insert M operator before KK
-            if (workflowPortion.get(workflowPortion.size() - 1).getOperator() != Symbol.M) {
-                workflowPortion.add(new Token(Symbol.M));
-            }
-
-            // focus on text field by mouse
-            if (handContext != HandContext.ON_MOUSE) {
-                workflowPortion.add(new Token(Symbol.H));
-                handContext = HandContext.ON_MOUSE;
-                workflowPortion.add(new Token(Symbol.M));
-            }
-            workflowPortion.add(new Token(Symbol.P));
-            workflowPortion.add(new Token(Symbol.BB));
-            workflowPortion.add(new Token(Symbol.M));
-            workflowPortion.add(new Token(Symbol.H));
-            handContext = HandContext.ON_KEYBOARD;
-            workflowPortion.add(new Token(Symbol.M));
-        }
-        token.setMultiplier(Regex.get(token.getDescription(), "\"[\\w\\s]+\"").replace("\"", "").length());
-        workflowPortion.addAll(Wrapper.wrap(token));
-
-        return workflowPortion;
-    }
-
-    private static List<Token> handleK(Token token) {
-        List<Token> workflowPortion = new ArrayList<>();
-
-        if (workflowPortion.get(workflowPortion.size() - 1).getOperator() != Symbol.M) {
-            workflowPortion.add(new Token(Symbol.M));
-        }
-
-        if (handContext != HandContext.ON_KEYBOARD) {
-            workflowPortion.add(new Token(Symbol.H));
-            handContext = HandContext.ON_KEYBOARD;
-            workflowPortion.add(new Token(Symbol.M));
-        }
-
-        workflowPortion.addAll(Wrapper.wrap(token));
-        workflowPortion.add(new Token(Symbol.T));
-
-        return workflowPortion;
-    }
-
-    private static void handleF(Token token) {
-        // init focused element
-        focusedElement = Regex.get(token.getDescription(), "\"[\\w\\s]+\"").replace("\"", "");
     }
 }
