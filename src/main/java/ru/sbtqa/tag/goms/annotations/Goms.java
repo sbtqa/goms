f758afcc6d49e38c24689fea39badfe12dcdd2d1
Konstantin Maltsev <mkypers@gmail.com>
2017-09-28 11:05:25 +0700

    add java doc to goms annotation

--- src/main/java/ru/sbtqa/tag/goms/annotations/Goms.java
+++ src/main/java/ru/sbtqa/tag/goms/annotations/Goms.java
@@ -6,11 +6,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import ru.sbtqa.tag.goms.objects.Symbol;
 
+/**
+ * Declare method as goms operator
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Goms {
 
+    // Sequence of the operators to add before this
     public Symbol[] prelims() default {};
+    // This operator's symbol
     public Symbol symbol();
+    // True if user need to transfer hands to the object which is not a mouse or a keyboard
     public boolean selfContext() default false;
 }
